1ec281d7b9abdc714c5f76ba12a6d1fb7fb9d533
Gabriela_Pineiro <gabriela.pineiro@factoriaf5.org>
2023-04-27 14:12:02 +0200

    first commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<nav class="top-bar">
+		<div class="top-bar-div">Coder App</div>
+	</nav>
+
+</main>
+
+<style>
+	.top-bar {
+		background-color: blue;
+		height: 80px;
+		width: 100%;
+		display:flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.top-bar-div{
+		color:white;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
