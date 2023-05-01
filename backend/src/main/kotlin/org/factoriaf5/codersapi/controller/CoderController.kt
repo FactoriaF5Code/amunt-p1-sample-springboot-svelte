e6c96a6c348bbad048e0edb57e37c5fffeaebde7
Gabriela_Pineiro <gabriela.pineiro@factoriaf5.org>
2023-05-01 13:34:17 +0200

    created a postTest and Controller test

--- backend/src/main/kotlin/org/factoriaf5/codersapi/controller/CoderController.kt
+++ backend/src/main/kotlin/org/factoriaf5/codersapi/controller/CoderController.kt
@@ -4,6 +4,8 @@ import org.factoriaf5.codersapi.repositories.Coder
 import org.factoriaf5.codersapi.repositories.CoderRepository
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 @CrossOrigin
 @RestController
@@ -11,5 +13,10 @@ class CoderController(private val coderRepo: CoderRepository) {
     @GetMapping("/api/coders")
     fun getCoders(): List<Coder> {
         return coderRepo.findAll()
+    }
+
+    @PostMapping("/api/coders")
+    fun createNewCoder(@RequestBody coder:Coder): Coder?{
+        return coderRepo.save(coder)
     }
 }
--- backend/src/test/kotlin/org/factoriaf5/codersapi/BackendApplicationTests.kt
+++ backend/src/test/kotlin/org/factoriaf5/codersapi/BackendApplicationTests.kt
@@ -1,7 +1,12 @@
 package org.factoriaf5.codersapi
 
+
+import com.fasterxml.jackson.databind.ObjectMapper
+
 import org.factoriaf5.codersapi.repositories.Coder
 import org.factoriaf5.codersapi.repositories.CoderRepository
+
+import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -9,7 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus.OK
+import org.springframework.http.MediaType
+import java.util.*
 
 @SpringBootTest(
         classes = [BackendApplication::class],
@@ -17,22 +26,27 @@ import org.springframework.http.HttpStatus.OK
 )
 class BackendApplicationTests {
 
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
     @Autowired //injecta una dependecia
     lateinit var restTemplate: TestRestTemplate
 
 
     @Autowired
-    lateinit var repository : CoderRepository
+    lateinit var repository: CoderRepository
 
     @BeforeEach //ejecuta antes de cada test
     fun setUp() {
         repository.deleteAll()
     }
 
+
     @Test
     fun getCoders() {
         //given
-        val coder = Coder(name = "Pepe", favoriteLanguage ="PHP", imageUrl ="example.jpg")
+        val coder = Coder(name = "Pepe", favoriteLanguage = "PHP", imageUrl = "example.jpg")
         repository.save(coder)
 
         // when
@@ -43,4 +57,21 @@ class BackendApplicationTests {
         assertEquals(response.body?.size, 1)
         assertEquals(response.body?.get(0), coder)
     }
+
+    @Test
+    fun createNewCoder() {
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+        val requestBody = Coder("Gabi", "Kotlin", "example2.jpg")
+        val request = HttpEntity(requestBody, headers)
+        val response = restTemplate.postForEntity("/api/coders", request, Coder::class.java)
+        val id = response.getBody()!!.id!!
+        assertEquals(OK, response.statusCode)
+        val coder = repository.findById(id).get()
+        assertEquals(coder.name,"Gabi" )
+        assertEquals(coder.favoriteLanguage,"Kotlin" )
+        assertEquals(coder.imageUrl,"example2.jpg" )
+    }
 }
+
+
